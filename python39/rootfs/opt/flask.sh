549390b12eb804787f9d1adcdea6c140abdf50db
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2022-03-30 17:41:00 +0200

    python39: fix Flask launch.
    We go in the local directory before running the webserver in order to avoid to break local path resolution.

--- python39/rootfs/opt/flask.sh
+++ python39/rootfs/opt/flask.sh
@@ -3,5 +3,5 @@
 # We load all environment variable defined in the .env file of the project since they are needed for running the Flask
 # server.
 export $(cat /var/www/.env | xargs)
-/usr/local/bin/python /var/www/server.py &
+cd /var/www && /usr/local/bin/python server.py &
 echo $! > /run/flask.pid
