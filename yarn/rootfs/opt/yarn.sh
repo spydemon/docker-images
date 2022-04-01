2c4892fed5153b2f720ea7c09033476a0dd3b9f9
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2022-04-01 12:08:27 +0200

    python39: fix Flash stop.
    The PID write in the flask.pid file was the one of the `cd` command and not of the `python` one. It was thus impossible for OpenRC to stop the server.

--- yarn/rootfs/opt/yarn.sh
+++ yarn/rootfs/opt/yarn.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # We load all environment variable defined in the .env file of the project since they are needed for running the Flask
 # server.
 export $(cat /var/www/.env | xargs)
-cd /var/www && /usr/bin/yarn dev &
-echo $! > /run/yarn.pid
+cd /var/www
+/usr/local/bin/python server.py &
+echo $! > /run/flask.pid
